c972f9137d1edf6e8d3bde292a5f749472e13b7a
cszzang89@gamail.com <cszzang89@gmail.com>
2022-08-09 13:02:42 +0900

    Address.java. Regex.java add

---
+++ src/main/java/com/project/carrot/domain/address/entity/Address.java
@@ -0,0 +1,26 @@
+package com.project.carrot.domain.address.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="ADDRESS")
+@Getter
+public class Address {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String district;
+    private String town;
+    private String dong;
+
+    public Address() {
+    }
+
+    public Address(String district, String town, String dong) {
+        this.district = district;
+        this.town = town;
+        this.dong = dong;
+    }
+}
---
+++ src/main/java/com/project/carrot/utlis/Regex.java
@@ -0,0 +1,6 @@
+package com.project.carrot.utlis;
+
+public class Regex {
+
+    final String loginIdRegex = "^[a-z]+[a-z0-9]{5,19}$"; //영문+숫자만 가능
+}
