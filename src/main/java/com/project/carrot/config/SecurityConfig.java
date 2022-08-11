ae9599d1490d0742990afec46f1e11b23ce55f55
cszzang89@gamail.com <cszzang89@gmail.com>
2022-08-12 08:46:15 +0900

    add webSecurityCustomizer() in SecurityConfig

--- src/main/java/com/project/carrot/config/SecurityConfig.java
+++ src/main/java/com/project/carrot/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.project.carrot.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,6 +20,16 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated();
         return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer()throws Exception{
+        return (web) ->{
+            web.ignoring()
+                    .requestMatchers(PathRequest
+                            .toStaticResources() //static resources 접근 허용
+                            .atCommonLocations());
+        };
     }
 
 }
