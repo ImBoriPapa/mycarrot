ac914e60bf06bf8c31958e1763fad164d2ed8368
cszzang89@gamail.com <cszzang89@gmail.com>
2022-08-31 01:18:27 +0900

    create AddressRepository

---
+++ src/main/java/com/project/carrot/domain/address/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package com.project.carrot.domain.address.repository;
+
+import com.project.carrot.domain.address.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address,Long> {
+
+}
